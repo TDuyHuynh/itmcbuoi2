29a60ee4b5540597fec1179206e82a77245bc8ad
TDuyHuynh <Tanduyhuynh221006@gmail.com>
2024-11-03 20:59:02 +0700

    Add files via upload

---
+++ 25.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,i=1;
+    cout <<"nhap 1 so nguyen ";
+    cin >>n;
+    while (i<=n)
+    {
+        cout <<i<<" ";
+        i+=2;
+    }
+}
